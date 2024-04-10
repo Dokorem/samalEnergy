263ee67600bbda87a68fff5a7873a1db1ed046e5
dokorem <150316580+Dokorem@users.noreply.github.com>
2024-04-10 11:21:26 +0600

    Delete src/main directory

--- src/main/java/com/example/Samal_Energy/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Samal_Energy.repositories;
-
-import com.example.Samal_Energy.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
--- src/main/java/com/example/Samal_Energy/repositories/NewsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Samal_Energy.repositories;
-
-
-import com.example.Samal_Energy.models.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
